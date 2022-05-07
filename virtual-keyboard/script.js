5e78588a0fc983286cc6e75dfb7ee3ccb86b4c66
alpekhota <alekspekhota@gmail.com>
2022-05-07 10:53:36 +0200

    feat: create textarea in scrit.js

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -4,3 +4,11 @@ const title = document.createElement('div');
 title.className = 'title';
 title.textContent = 'My virtual-keyboard';
 document.body.appendChild(title);
+
+/*--------create textarea--------*/
+
+const input = document.createElement('div');
+input.className = 'textarea';
+input.rows = '20';
+input.cols = '50';
+document.body.appendChild(input);
